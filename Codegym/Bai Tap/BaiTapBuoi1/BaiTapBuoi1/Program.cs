66689d300e841669737884b0fc390537d2141297
tungnguyen2398 <admiralkurayami@gmail.com>
2023-05-16 21:34:42 +0700

    xong bai tap

---
+++ Codegym/Bai Tap/BaiTapBuoi1/BaiTapBuoi1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        ChuyenDoiTienTe();
+    }
+
+    private static void HienThiLoiChao() 
+    {
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Xin Chao: " + yourName);
+    }
+
+    private static void ChuyenDoiTienTe()
+    {
+        Console.WriteLine("Nhap So USD can chuyen doi: ");
+        string usdInput = Console.ReadLine();
+
+        if (int.TryParse(usdInput, out int usd))
+        {
+            int tiGiaQuyDoi = 23000;
+            int tienQuyDoi = usd * tiGiaQuyDoi;
+            Console.WriteLine("So Tien Quy doi la: " + tienQuyDoi);
+        }
+        else
+        {
+            Console.WriteLine("Nhap vao mot so USD hop le.");
+        }
+    }
+}
