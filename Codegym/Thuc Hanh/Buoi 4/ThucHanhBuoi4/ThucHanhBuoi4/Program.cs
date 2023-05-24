ab51b0b0e11575a8f8afd196f2e7e81c943e3607
tungnguyen2398 <admiralkurayami@gmail.com>
2023-05-23 19:55:18 -0700

    ThucHanhBuoi4

---
+++ Codegym/Thuc Hanh/Buoi 4/ThucHanhBuoi4/ThucHanhBuoi4/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace ThucHanhBuoi4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimGiaTriLonNhat();
+        }
+
+        static void TinhTong() 
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            Console.WriteLine(total.ToString());
+        }
+
+        static void TimGiaTriTrongMang() 
+        {
+            string[] students = { "Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily" };
+            Console.WriteLine("Enter a name’s student:");
+            string input_name = Console.ReadLine();
+            bool isExist = false;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Equals(input_name))
+                {
+                    Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist)
+            {
+                Console.WriteLine("Not found" + input_name + " in the list.");
+            }
+
+        }
+
+        static void TimGiaTriLonNhat() 
+        {
+            int size;
+            int[] array;
+            do
+            {
+                Console.WriteLine("Enter a size:");
+                size = Int32.Parse(Console.ReadLine());
+                if (size > 20)
+                    Console.WriteLine("Size should not exceed 20");
+            } while (size > 20);
+
+            array = new int[size];
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.WriteLine("Enter element" + (i + 1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("Property list: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j] + "\t");
+            }
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    index = j + 1;
+                }
+            }
+            Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
+        }
+    }
+}
