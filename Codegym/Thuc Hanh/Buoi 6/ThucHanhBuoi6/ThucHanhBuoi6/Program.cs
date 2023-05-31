7d245b7aa2f64d8aab93201ef0e1f4ad18c750b6
tungnguyen2398 <admiralkurayami@gmail.com>
2023-05-31 06:00:46 -0700

    thuc hanh buoi 6

---
+++ Codegym/Thuc Hanh/Buoi 6/ThucHanhBuoi6/ThucHanhBuoi6/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace ThucHanhBuoi6
+{
+    public class Program
+    {
+        /**public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+
+        }**/
+
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+        }
+    }
+
+
+
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+
+       
+    }
+
+    public abstract class Animal
+    {
+        protected string Weight { get; set; }
+        protected string Height { get; set; }
+
+        public Animal(string weight, string height)
+        {
+            this.Weight = weight;
+            this.Height = height;
+        }
+        public abstract void PrintInfo();
+    }
+
+    class Cat : Animal
+    {
+        private string Name { get; set; }
+        public Cat(string weight, string height, string name)
+        : base(weight, height)
+        {
+            this.Name = name;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}", this.Weight, this.Height, this.Name);
+        }
+    }
+}
