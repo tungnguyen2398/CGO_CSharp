bb81650f4234d5743cb603331d9e1f56bba0c9a8
tungnguyen2398 <admiralkurayami@gmail.com>
2023-06-09 00:30:30 -0700

    Dem ky tu
    bai tap dem ky tu buoi 5

---
+++ Codegym/Bai Tap/Bai Tap Buoi 5 Ham/DemKyTuXuatHien/DemKyTU/DemKyTU/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DemKyTU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao mot chuoi:");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Nhap vao mot ky tu can dem so lan xuat hien:");
+            char inputChar = char.Parse(Console.ReadLine());
+
+            int count = 0;
+            foreach (char c in inputString)
+            {
+                if (c == inputChar)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("So lan xuat hien cua ky tu {0} trong chuoi '{1}' la {2}.", inputChar, inputString, count);
+            Console.ReadLine();
+        }
+    }
+}
