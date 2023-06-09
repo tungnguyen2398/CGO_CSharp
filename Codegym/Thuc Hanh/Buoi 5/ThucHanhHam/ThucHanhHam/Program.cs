6cc74af1ed47d7efd6b33de2a8fdd9bcc443b06f
tungnguyen2398 <admiralkurayami@gmail.com>
2023-06-08 23:42:35 -0700

    thuc hanh chuyen doi nhiet do
    thuc hanh ham

---
+++ Codegym/Thuc Hanh/Buoi 5/ThucHanhHam/ThucHanhHam/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ThucHanhHam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Chuyen F thanh C");
+            Console.WriteLine("2.Chuyen C thanh F");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap gia tri can chuyen: ");
+            
+            double value = int.Parse(Console.ReadLine());
+
+            if(choice == 2 )
+            {
+                Console.Write((9.0 / 5) * value + 32);
+            } else if (choice == 1 )
+            {
+                Console.Write((5.0 / 9) * (value - 32));
+            }
+            Console.ReadKey();
+        }
+    }
+}
